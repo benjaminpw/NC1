752eaf9ffa17cd3b9f981bb71dad39144d0b7960
benjopw <benyaminpurbowasito@gmail.com>
2019-05-16 09:06:47 +0700

    Initial Commit

---
+++ NC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1
+//
+//  Created by Benjamin Purbowasito on 16/05/19.
+//  Copyright © 2019 iosda Ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
