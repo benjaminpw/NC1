92ff123ae36d6305941474ef61782945a932aa83
benjopw <benyaminpurbowasito@gmail.com>
2019-05-16 19:41:10 +0700

    udah bisa bikin satu tone di satu button, tapi interval nya  masih belom nemu

---
+++ NC1/Round3Dbutton.swift
@@ -0,0 +1,23 @@
+//
+//  Round3Dbutton.swift
+//  NC1
+//
+//  Created by Benjamin Purbowasito on 16/05/19.
+//  Copyright © 2019 iosda Ben. All rights reserved.
+//
+
+import UIKit
+
+
+class Round3Dbutton: UIButton {
+    override func awakeFromNib() {
+        layer.borderWidth = 2
+        layer.cornerRadius = 26
+        layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        layer.shadowRadius = 15
+    }
+    
+    
+
+}
--- NC1/ViewController.swift
+++ NC1/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var musicEffect: AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let musicFile = Bundle.main.path(forResource: "1", ofType: ".m4a")
+        do {
+            try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
+        }
+        catch {
+            print(error)
+        }
+        
+        
     }
-
-
+    @IBAction func playTone1(_ sender: Any) {
+        musicEffect.play()
+        }
 }
-
