bafd2b4c79799291be5b7c2c391d810b70e05380
benjopw <benyaminpurbowasito@gmail.com>
2019-05-17 13:33:25 +0700

    bisa masukin suara lebih dari satu terus durasi music nya bisa lebih pendek

--- NC1/ViewController.swift
+++ NC1/ViewController.swift
@@ -12,12 +12,27 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var musicEffect: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect1: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect2: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect3: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect4: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect5: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect6: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect7: AVAudioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let musicFile = Bundle.main.path(forResource: "1", ofType: ".m4a")
+        let musicFile = Bundle.main.path(forResource: "test", ofType: ".m4a")
         do {
             try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
+        }
+        catch {
+            print(error)
+        }
+        
+        let musicFile1 = Bundle.main.path(forResource: "2", ofType: ".m4a")
+        do {
+            try musicEffect1 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile1!))
         }
         catch {
             print(error)
@@ -27,5 +42,8 @@ class ViewController: UIViewController {
     }
     @IBAction func playTone1(_ sender: Any) {
         musicEffect.play()
-        }
+}
+    @IBAction func playTone2(_ sender: Round3Dbutton) {
+        musicEffect1.play()
+    }
 }
