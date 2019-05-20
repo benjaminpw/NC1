d6acd76d6c875f0ea75f47b8960e0a1e46a9b40d
benjopw <benyaminpurbowasito@gmail.com>
2019-05-20 13:08:42 +0700

    udah assign semua tone nya ke button

--- NC1/ViewController.swift
+++ NC1/ViewController.swift
@@ -25,42 +25,24 @@ class ViewController: UIViewController {
         let musicFile = Bundle.main.path(forResource: "1", ofType: ".mp3")
         let musicFile1 = Bundle.main.path(forResource: "2", ofType: ".mp3")
         let musicFile2 = Bundle.main.path(forResource: "3", ofType: ".mp3")
-//        let musicFile3 = Bundle.main.path(forResource: "<#T##String?#>", ofType: ".mp3")
-//        let musicFile4 = Bundle.main.path(forResource: "<#T##String?#>", ofType: ".mp3")
-//        let musicFile5 = Bundle.main.path(forResource: "<#T##String?#>", ofType: ".mp3")
-//        let musicFile6 = Bundle.main.path(forResource: "<#T##String?#>", ofType: ".mp3")
-//        let musicFile7 = Bundle.main.path(forResource: "<#T##String?#>", ofType: ".mp3")
+        let musicFile3 = Bundle.main.path(forResource: "4", ofType: ".mp3")
+        let musicFile4 = Bundle.main.path(forResource: "5", ofType: ".mp3")
+        let musicFile5 = Bundle.main.path(forResource: "6", ofType: ".mp3")
+        let musicFile6 = Bundle.main.path(forResource: "7", ofType: ".mp3")
+        let musicFile7 = Bundle.main.path(forResource: "8", ofType: ".mp3")
         do {
             try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
             try musicEffect1 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile1!))
             try musicEffect2 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile2!))
-//            try musicEffect3 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile3!))
-//            try musicEffect4 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile4!))
-//            try musicEffect5 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile5!))
-//            try musicEffect6 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile6!))
-//            try musicEffect7 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile7!))
+            try musicEffect3 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile3!))
+            try musicEffect4 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile4!))
+            try musicEffect5 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile5!))
+            try musicEffect6 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile6!))
+            try musicEffect7 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile7!))
         }
         catch {
             print(error)
         }
-        
-//        let musicFile1 = Bundle.main.path(forResource: "", ofType: ".mp3")
-//        do {
-//            try musicEffect1 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile1!))
-//        }
-//        catch {
-//            print(error)
-//        }
-//
-//        let musicFile2 = Bundle.main.path(forResource: "", ofType: ".mp3")
-//        do {
-//            try musicEffect2 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile2!))
-//        }
-//        catch {
-//            print(error)
-//        }
-        
-        
     }
     @IBAction func playTone1(_ sender: Any) {
         if musicEffect.isPlaying {
@@ -69,7 +51,7 @@ class ViewController: UIViewController {
         musicEffect.currentTime = 0
         musicEffect.play()
 }
-    @IBAction func playTone2(_ sender: Round3Dbutton) {
+    @IBAction func playTone2(_ sender: Any) {
         if musicEffect1.isPlaying {
             musicEffect1.pause()
     }
@@ -77,13 +59,41 @@ class ViewController: UIViewController {
     musicEffect1.play()
 
 }
-    
     @IBAction func playTone3(_ sender: Any) {
         if musicEffect2.isPlaying {
             musicEffect2.pause()
         }
         musicEffect2.currentTime = 0
         musicEffect2.play()
+    }
+    
+    @IBAction func playTone4(_ sender: Any) {
+        if musicEffect3.isPlaying {
+            musicEffect3.pause()
+        }
+        musicEffect3.currentTime = 0
+        musicEffect3.play()
+    }
+    @IBAction func playTone5(_ sender: Any) {
+        if musicEffect4.isPlaying {
+            musicEffect4.pause()
+        }
+        musicEffect4.currentTime = 0
+        musicEffect4.play()
+    }
+    @IBAction func playTone6(_ sender: Any) {
+        if musicEffect5.isPlaying {
+            musicEffect5.pause()
+        }
+        musicEffect6.currentTime = 0
+        musicEffect6.play()
+    }
+    @IBAction func playTone7(_ sender: Any) {
+        if musicEffect7.isPlaying {
+            musicEffect7.pause()
+        }
+        musicEffect7.currentTime = 0
+        musicEffect7.play()
     }
 }
 
